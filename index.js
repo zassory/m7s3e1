7546d6043cd4f1bf9fc9285fb96d84a8acc018ab
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-08-17 18:05:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const { Pool } = require('pg');
+const Cursor = require('pg-cursor');
+const assert = require('assert');
+
+const pool = new Pool({
+    user: 'zasory',
+    host: 'localhost',
+    database: 'practica_db',
+    password: 'Hakxf2n5$',
+    port: 5432,
+});
+
+const buscarMayores = async() => {
+
+    try{
+
+        const client = await pool.connect();
+        const cursor = client.query(new Cursor("SELECT * FROM estudiantes WHERE edad > '25'"));
+
+        let rows = await cursor.read(2);
+        //console.log(row);
+        while(rows.length){
+            console.table(rows);
+            console.log(rows.length);
+            console.assert(rows.length == 2);
+            rows = await cursor.read(2);
+        }
+
+    }catch(error){
+        if(error.code == '42601'){
+             console.log("\n ERROR! \n Error de Sintaxis\n");
+         }
+         if(error.code == '25P01'){
+             console.log("\n ERROR! \n Password inválido\n");
+         }else{
+             console.error(error.stack);
+             console.log(error.code);
+        }
+    }
+    finally{
+        console.log('Terminado');
+    }
+}
+
+//buscarMayores();
+
+const buscarEstudiantesDesc = async() => {
+
+    try{
+
+        const client = await pool.connect();
+        const cursor = client.query(new Cursor("SELECT * FROM estudiantes ORDER BY apellidos DESC"));
+
+        let rows = await cursor.read(2);
+        //console.log(row);
+        while(rows.length){
+            console.table(rows);
+            console.log(rows.length);
+            console.assert(rows.length == 2);
+            rows = await cursor.read(2);
+        }
+
+    }catch(error){
+        if(error.code == '42601'){
+             console.log("\n ERROR! \n Error de Sintaxis\n");
+         }
+         if(error.code == '25P01'){
+             console.log("\n ERROR! \n Password inválido\n");
+         }else{
+             console.error(error.stack);
+             console.log(error.code);
+        }
+    }
+    finally{
+        console.log('Terminado');
+    }
+}
+
+//buscarEstudiantesDesc();
+
+const buscarCursosDisponibles = async() => {
+
+    try{
+
+        const client = await pool.connect();
+        const cursor = client.query(new Cursor("SELECT * FROM cursos"));
+
+        let rows = await cursor.read(2);
+        //console.log(row);
+        while(rows.length){
+            console.table(rows);
+            console.log(rows.length);
+            console.assert(rows.length == 2);
+            rows = await cursor.read(2);
+        }
+
+    }catch(error){
+        if(error.code == '42601'){
+             console.log("\n ERROR! \n Error de Sintaxis\n");
+         }
+         if(error.code == '25P01'){
+             console.log("\n ERROR! \n Password inválido\n");
+         }else{
+             console.error(error.stack);
+             console.log(error.code);
+        }
+    }
+    finally{
+        console.log('Terminado');
+    }
+}
+
+buscarCursosDisponibles();
+
